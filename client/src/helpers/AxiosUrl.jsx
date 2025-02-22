d2d90bbfe27a5771e8be2594b53f833b29abcdd0
Code Sherrif <codesherrif@gmail.com>
2025-02-22 18:32:39 +0300

    Delete client directory

--- client/src/helpers/AxiosUrl.jsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const axios_url = axios.create({
-  baseURL: "http://localhost:5000/api/", // Ensure the base URL is correct for your local setup
-});
-
-export default axios_url;
