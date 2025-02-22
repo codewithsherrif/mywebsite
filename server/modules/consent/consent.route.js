68d0cc5604e2a93a2ac0fbe87937c6e0b5ffc542
Code Sherrif <codesherrif@gmail.com>
2025-02-22 18:31:56 +0300

    Delete server directory

--- server/modules/consent/consent.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const saveConsent = require("./controllers/Consent");
-const consentRoutes = express.Router();
-
-consentRoutes.post("/", saveConsent);
-
-module.exports = consentRoutes;
